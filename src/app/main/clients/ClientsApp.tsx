050e8927119e8e056edd8b5bff7ae7e8255944fc
Alexander Serrano <cyasam7@gmail.com>
2025-03-23 02:31:35 -0600

    Update ESLint and Prettier configurations for improved code style consistency, adjust tab width in Prettier settings, and refactor ClientsApp component for better readability and organization of query handling and component structure.

--- src/app/main/clients/ClientsApp.tsx
+++ src/app/main/clients/ClientsApp.tsx
@@ -1,9 +1,9 @@
 import FusePageSimple from '@fuse/core/FusePageSimple';
 import { styled } from '@mui/material/styles';
 import { Container } from '@mui/material';
-import { ClientList } from './ClientDetail/ClientList';
 import { ClientService } from 'src/app/shared/services/ClientService';
 import { useQuery } from 'react-query';
+import { ClientList } from './ClientDetail/ClientList';
 import { FormClientValues } from './Forms/NewClientForm/types';
 
 const Root = styled(FusePageSimple)(({ theme }) => ({
@@ -12,36 +12,41 @@ const Root = styled(FusePageSimple)(({ theme }) => ({
 		borderBottomWidth: 1,
 		borderStyle: 'solid',
 		borderColor: theme.palette.divider
-	},
+	}
 }));
 
 /**
  * The ContactsApp page.
  */
 export function ClientsApp() {
-
-	const { data = [], isLoading, refetch } = useQuery({
-		queryKey: ["clients"],
+	const {
+		data = [],
+		isLoading,
+		refetch
+	} = useQuery({
+		queryKey: ['clients'],
 		queryFn: () => ClientService.getByQuery()
-	})
-
+	});
 
 	async function handleSaveClient(data: FormClientValues, id?: string) {
-		await ClientService.save(data, id)
-		await refetch()
+		await ClientService.save(data, id);
+		await refetch();
 	}
 
-
 	async function handleDeleteClient(id: string) {
-		await ClientService.remove(id)
-		await refetch()
+		await ClientService.remove(id);
+		await refetch();
 	}
 
 	return (
 		<Root
 			content={
-				<Container sx={{ py: 4, maxWidth: "1400px !important" }}>
-					<ClientList clients={data} onSaveClient={handleSaveClient} onDeleteClient={handleDeleteClient} />
+				<Container sx={{ py: 4, maxWidth: '1400px !important' }}>
+					<ClientList
+						clients={data}
+						onSaveClient={handleSaveClient}
+						onDeleteClient={handleDeleteClient}
+					/>
 				</Container>
 			}
 		/>
